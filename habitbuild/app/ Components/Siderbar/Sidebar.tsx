30528cf0700155ecbada235d55a75cba97479090
dkimb0526 <49534232+dkimb0526@users.noreply.github.com>
2024-03-07 17:52:28 +0000

    added new files and components

--- habitbuild/app/ Components/Siderbar/Sidebar.tsx
+++ habitbuild/app/ Components/Siderbar/Sidebar.tsx
@@ -1,14 +1,17 @@
 "use client";
 import React from "react"
 import styled from "styled-components";
+import {useGlobalState } from "@/app/context/globalProvider";
 
 function Sidebar(){
+const{ theme } = useGlobalState();
+
     return(
         <SidebarStyled>Sidebar</SidebarStyled>
     )
 }
 
 const SidebarStyled = styled.nav`
-    
+
 `;
 export default Sidebar
