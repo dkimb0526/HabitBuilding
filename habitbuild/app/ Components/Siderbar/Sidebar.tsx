dd3310f1104782ae21596e070f8c8a17b4e170d7
dkimb0526 <49534232+dkimb0526@users.noreply.github.com>
2024-03-08 17:21:23 +0000

    displayed sidebar

--- habitbuild/app/ Components/Siderbar/Sidebar.tsx
+++ habitbuild/app/ Components/Siderbar/Sidebar.tsx
@@ -15,7 +15,7 @@ const SidebarStyled = styled.nav`
     position: relative;
     width: ${(props) => props.theme.sidebarWidth};
     background-color : ${(props) => props.theme.colorBg2};
-    border-right: 2px solid ${(props) => props.theme.borderColor2};
+    border: 2px solid ${(props) => props.theme.borderColor2};
     border-radidus: 1rem;
 `;
 export default Sidebar
