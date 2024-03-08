ee66324b05af67dedf423b09ee6698d0f6afcc97
dkimb0526 <49534232+dkimb0526@users.noreply.github.com>
2024-03-08 17:33:42 +0000

    import avatar profile pic and display

--- habitbuild/app/ Components/Siderbar/Sidebar.tsx
+++ habitbuild/app/ Components/Siderbar/Sidebar.tsx
@@ -2,12 +2,20 @@
 import React from "react"
 import styled from "styled-components";
 import {useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
 
 function Sidebar(){
 const{ theme } = useGlobalState();
 
     return(
-        <SidebarStyled theme = { theme }>Sidebar</SidebarStyled>
+        <SidebarStyled theme = { theme }>
+            Sidebar
+            <div className = "profile">
+                <div className="profile-overlay">
+                </div>
+                <Image width = {70} height={70} src = "/luffy.png" alt = "profile" />
+            </div>
+            </SidebarStyled>
     )
 }
 
