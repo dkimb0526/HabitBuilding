974d235fa418f5697ef817378adfe002d865d472
dkimb0526 <49534232+dkimb0526@users.noreply.github.com>
2024-03-12 16:49:37 +0000

    profile and profile overlay styling

--- habitbuild/app/ Components/Siderbar/Sidebar.tsx
+++ habitbuild/app/ Components/Siderbar/Sidebar.tsx
@@ -68,5 +68,39 @@ const SidebarStyled = styled.nav`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+
+    color: ${(props) => props.theme.colorGrey3};
+
+    .profile{
+        margin: 1.5rem;
+        padding: 1rem 0.8rem;
+        position: relative;
+
+        border-radius: 1rem;
+        cursor: pointer;
+
+        font-weight:500;
+        color: ${(props) => props.theme.colorWhite};
+
+        display: flex;
+        align-items: center;
+
+        .profile-overlay{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            backdrop-filter: blur(10px);
+            z-index: 0;
+            background: ${(props) => props.theme.colorBg3};
+            transition: all 0.55s linear;
+            border-radius: 1rem;
+
+            border: 2px solid ${(props) => props.theme.colorBg2};
+            opacity: 0.2;
+        }
+    }
+    
 `;
 export default Sidebar
