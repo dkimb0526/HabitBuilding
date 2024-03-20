9e6a62df6d29985d29cf1c860425899e716fef95
dkimb0526 <49534232+dkimb0526@users.noreply.github.com>
2024-03-20 16:29:30 +0000

    added css for tasks

--- habitbuild/app/Components/Tasks/Tasks.tsx
+++ habitbuild/app/Components/Tasks/Tasks.tsx
@@ -1,9 +1,27 @@
+"use client";
 import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
+
 
 function Tasks(){
+    const { theme } = useGlobalState();
     return (
-        <div>Tasks</div>
+        <TaskStyled theme= {theme}>Tasks</TaskStyled>
     )
 }
+
+const TaskStyled = styled.div`
+    width = 100%;
+    background-color: ${(props) => props.theme.colorBg2};
+    border: 2px solid ${(props) => props.theme.borderColor2};
+    border-radius: 1rem;
+    padding: 2rem;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar{
+        width: 0.5rem;
+    }
+`;
 
 export default Tasks;
